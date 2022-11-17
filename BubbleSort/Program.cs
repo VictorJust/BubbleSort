17ba1b06101449c9d58438db6f30105edb0950f6
VictorJust <victorjust09@gmail.com>
2022-11-17 14:33:03 +0300

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 5, 1, 4, 2, 8, 7, 10, 9 };
+        BubbleSort(arr);
+        Console.WriteLine("Sorted array");
+        foreach (int i in arr)
+            Console.Write(i + " ");
+    }
+    private static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n - 1; j++)
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+}
